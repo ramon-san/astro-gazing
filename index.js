1aff44d671ce62e17674c011b358445a38738042
Ramon Orraca <orraca1999@gmail.com>
2022-06-11 23:35:27 -0500

    feat Add CRUD functionality through RESTful routes.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path';
 import { fileURLToPath } from 'url'; // Needed to work with path.
+import methodOverride from 'method-override';
 import mongoose from 'mongoose';
 // We add our DB schema objects.
 import { Customer } from './models/customer.js'; // File extension is needed.
@@ -20,9 +21,12 @@ mongoose.connect('mongodb://localhost:27017/disale') // Mongo default port is 27
         console.log("DB connection error: " + err);
     });
 
-
 // We serve our static files to any request.
 app.use(express.static(path.join(__dirname, 'public'))); // __dirname gives us an absolute path.
+// Middleware to parse request as URL encoded.
+app.use(express.urlencoded({ extended: true }));
+// We call in the override middleware to allow form requests different than GET or POST.
+app.use(methodOverride('_method'));
 
 // We tell our app to use EJS.
 app.set('view engine', 'ejs');
@@ -34,9 +38,66 @@ app.get('/', (req, res) => {
     res.render('home', { title: "Home" });
 });
 
+// Render page indexing all customers.
+app.get('/customers', async (req, res) => {
+    const customers = await Customer.find({});
+    res.render('customers/index', { title: "Customer list", customers });
+});
+
+// Send message to create customers elsewhere.
 app.get('/make-customer', async (req, res) => {
-    await customer.save();
-    res.send(customer);
+    res.send('To make customers use seed...');
+});
+
+// Render page with form to add new customer.
+app.get('/customers/new', (req, res) => {
+    res.render('customers/new', { title: "New Client" });
+});
+
+// Add new customer to the DB.
+app.post('/customers', async (req, res) => {
+    const newCustomer = new Customer(req.body.customer);
+    await newCustomer.save();
+    res.redirect(`/customers/${newCustomer._id}`);
+});
+
+// Render page with form to edit new customer.
+app.get('/customers/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const customer = await Customer.findById(id);
+        res.render('customers/edit', { title: "Edit", customer });
+    } catch (err) {
+        console.log("Error: " + err)
+        res.redirect('/customers');
+    }
+});
+
+app.put('/customers/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updateCustomer = await Customer.findByIdAndUpdate(id, req.body.customer, { runValidators: true, new: true });
+        res.redirect(`/customers/${updateCustomer._id}`); // We need to await previous line for this to work.
+    } catch (e) {
+        res.send("ERROR: " + e);
+    }
+});
+
+// Delete given id.
+app.delete('/customers/:id', async (req, res) => {
+    const { id } = req.params;
+    await Customer.findByIdAndDelete(id);
+    res.redirect('/customers');
+});
+
+// Render page showing a specific customer.
+app.get('/customers/:id', async (req, res) => {
+    const customer = await Customer.findById(req.params.id);
+    try {
+        res.render('customers/show', { title: customer.name, customer });
+    } catch (err) {
+        console.log("Error: " + err);
+    }
 });
 
 app.listen(8080, () => {
